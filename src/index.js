d8674eedd167e8b12b1464b828d9ac5e36c2bedb
AjinkyaDaf <ajankya.daf72@gmail.com>
2023-10-20 18:27:22 +0530

    News App

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter , RouterProvider } from "react-router-dom";
-import "./index.css";
+import ReactDOM from 'react-dom/client';
+import Home from './views/Home/Home';
 
-import Home from "./views/Home/Home";
-import Contact from "./views/Contact/Contact";
-import About from "./views/About/About";
+const root =ReactDOM.createRoot(document.getElementById('root'));
 
-
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element: <Home/>,
-  },
-  {
-    path:"/about",
-    element: <About/>,
-  },
-  {
-    path:"/contact",
-    element: <Contact/>,
-  },
-]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RouterProvider router={router} />);
+root.render(
+<Home />
+);
